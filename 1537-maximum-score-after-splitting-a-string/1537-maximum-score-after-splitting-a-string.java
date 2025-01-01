c0f39e6f0325f4c069b502614613ea3e96df512b
Prasham Narayan <prasham18359@iiitd.ac.in>
2025-01-02 02:54:29 +0530

    Time: 2 ms (39.38%), Space: 41.5 MB (50.05%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.java
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.java
@@ -2,17 +2,14 @@ class Solution {
     public int maxScore(String s) {
         int[] zeros = new int[s.length()];
         int[] ones = new int[s.length()];
-        for(int i = 0;i<s.length();i++){
+        if(s.charAt(0) == '0'){
+            zeros[0]++;
+        }
+        if(s.charAt(s.length()-1) == '1'){
+            ones[s.length()-1]++;
+        }
+        for(int i = 1;i<s.length();i++){
             int end = s.length() - 1 -i;
-            if(i == 0){
-                if(s.charAt(i) == '0'){
-                    zeros[i]++;
-                }
-                if(s.charAt(end) == '1'){
-                    ones[end]++;
-                }
-                continue;
-            }
             zeros[i] = zeros[i-1];
             if(s.charAt(i) == '0'){
                 zeros[i]++;
