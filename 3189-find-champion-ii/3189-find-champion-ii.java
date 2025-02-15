213c1b0dbda417ced10c07a3f40e3ea321f10fef
Prasham Narayan <prasham18359@iiitd.ac.in>
2025-02-16 03:51:59 +0530

    Time: 1 ms (100%), Space: 45.7 MB (58.21%) - LeetHub

--- 3189-find-champion-ii/3189-find-champion-ii.java
+++ 3189-find-champion-ii/3189-find-champion-ii.java
@@ -1,16 +1,12 @@
 class Solution {
     public int findChampion(int n, int[][] edges) {
-        int[][] graph = new int[n][n];
+        int[] graph = new int[n];
         for (int[] edge : edges) {
-            graph[edge[1]][edge[0]]++;
+            graph[edge[1]]++;
         }
         ArrayList<Integer> ans = new ArrayList<>();
         for (int i = 0;i<n;i++) {
-            int count = 0;
-            for (int j = 0;j<n;j++) {
-                count += graph[i][j];
-            }
-            if (count == 0) {
+            if (graph[i] == 0) {
                 ans.add(i);
             }
         }
