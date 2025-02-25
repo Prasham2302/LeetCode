4674baf1179166dc8a4a988a5f6ddd07c1b99299
Prasham Narayan <prasham18359@iiitd.ac.in>
2025-02-25 21:04:47 +0530

    Time: 57 ms (37.41%), Space: 101.1 MB (97.19%) - LeetHub

--- 2164-two-best-non-overlapping-events/2164-two-best-non-overlapping-events.java
+++ 2164-two-best-non-overlapping-events/2164-two-best-non-overlapping-events.java
@@ -1,42 +1,50 @@
+class Times implements Comparable<Times>{
+    int time;
+    int isEnd;
+    int value;
+    Times(int time, int isEnd, int value) {
+        this.time = time;
+        this.isEnd = isEnd;
+        this.value = value;
+    }
+    @Override
+    public int compareTo(Times other) {        
+        if (this.time == other.time) {
+            return this.isEnd - other.isEnd;
+        }
+        return this.time - other.time;
+    }
+    @Override
+    public String toString() {
+        return time + " " + isEnd + " " + value;
+    }
+
+}
 class Solution {
     public int maxTwoEvents(int[][] events) {
-        Arrays.sort(events, (a,b) -> a[0] - b[0]);
-        int[][] dp = new int[2][events.length];
-        Arrays.fill(dp[0],-1);
-        Arrays.fill(dp[1],-1);
-        // System.out.println(Arrays.deepToString(events));
-        // System.out.println("1 "+ events[2][2]);
-        return solve(events, dp, 0, 0);
-    }
-    public int solve(int[][] events, int[][] dp, int idx, int k) {
-        // System.out.println("intial "+idx +" "+k +" "+Arrays.deepToString(dp));
-        if (k == 2 || idx >= events.length) return 0;
-        if (dp[k][idx] != -1) return dp[k][idx];
-        int nextIdx = upperBound(events, idx);
-        int includeVal = solve(events, dp, nextIdx, k+1);
-        int include = events[idx][2] + includeVal;
-        int exclude = solve(events, dp, idx+1, k);
-        dp[k][idx] = Math.max(include, exclude);
-        // System.out.println("include "+include +" exclude "+exclude +" "+includeVal +" "+events[idx][2] +" "+idx);
-        // System.out.println("later "+idx +" "+k +" "+Arrays.deepToString(dp) +" upperbound "+nextIdx);
-        return dp[k][idx];
-    }
-    public int upperBound(int[][] events, int idx) {
-        int target = events[idx][1];
-        int left = idx + 1;
-        int right = events.length-1;
-        int ans = events.length;
-        while (left <= right) {
-            // System.out.println("in UpperBound "+left +" "+right);
-            int mid = (right + left) / 2;
-            if (events[mid][0] > target) {
-                right = mid-1;
-                ans = mid;
-            } else {
-                left = mid+1;
-            }
+        Times[] times = new Times[2 * events.length];
+        int j = 0;
+        for (int[] event : events) {
+            times[j] = new Times(event[0], 1, event[2]);
+            j++;
+            times[j] = new Times(event[1]+1, 0, event[2]);
+            j++;
         }
-        // System.out.println("in UpperBound "+idx +" "+ans);
+        Arrays.sort(times);
+        // System.out.println(Arrays.toString(times));
+        int ans = 0;
+        int currMax = 0;
+        for (int i = 0;i<times.length;i++) {
+            Times time = times[i];
+            if (time.isEnd == 0) {
+                currMax = Math.max(currMax, time.value);
+            }
+            else {
+                ans = Math.max(currMax + time.value, ans);
+            }
+            // System.out.println(time.toString() +" || "+currMax+" "+ans);
+        }
         return ans;
+
     }
 }
