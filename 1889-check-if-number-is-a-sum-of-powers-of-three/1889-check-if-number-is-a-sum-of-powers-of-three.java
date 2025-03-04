1ca0fc546fdb441e532de5d292675ca21527fff9
Prasham Narayan <prasham18359@iiitd.ac.in>
2025-03-05 00:51:53 +0530

    Time: 0 ms (100%), Space: 40.6 MB (32.85%) - LeetHub

---
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean checkPowersOfThree(int n) {
+        if (n <= 1) return true;
+        else if (n == 2 || n % 3 == 2) return false;
+        return checkPowersOfThree(n / 3);
+    }
+}
