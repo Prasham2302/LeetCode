9beb5065c650a6bf8910ae244870689987da2620
Prasham Narayan <prasham18359@iiitd.ac.in>
2025-03-24 15:23:06 +0530

    Time: 0 ms (100%), Space: 41.8 MB (52.67%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -15,17 +15,13 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if(root == null){
-            return true;
-        } 
+        if (root == null) return true;
         return helper(root.left, root.right);
     }
-    public boolean helper(TreeNode left, TreeNode right){
-        if(left == null && right == null)
-            return true;
-        if(left == null || right == null || left.val != right.val){
-            return false;
-        }
+    public boolean helper(TreeNode left, TreeNode right) {
+        if (left == null && right == null) return true;
+        if (left == null || right == null) return false;
+        if (left.val != right.val) return false;
         return helper(left.left, right.right) && helper(left.right, right.left);
     }
 }
